9d38f34259dcff2c4b1d2ad24221d7ebeda804a9
Shivam Maurya <109658264+ShivamManiMaurya@users.noreply.github.com>
2022-07-31 21:32:37 +0530

    Player movement
    The player can now move left or right, without animation.

--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -1,18 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] float totoSpeed = 5;
+
+    Vector2 moveInput;
+    Rigidbody2D totoRigidbody;
+
     void Start()
     {
+        totoRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        Run();
+    }
+
+    void OnMove(InputValue value)
+    {
+        moveInput = value.Get<Vector2>(); 
         
     }
 
-    // Update is called once per frame
-    void Update()
+    void Run()
     {
-        
+        Vector2 playerVelocity = new Vector2(totoSpeed * moveInput.x, totoRigidbody.velocity.y);
+        totoRigidbody.velocity = playerVelocity;
+        Debug.Log(totoRigidbody.velocity);
+
+        bool val = totoRigidbody.velocity.x > 0;
+        Debug.Log(val);
+
+        if (totoRigidbody.velocity.x >= 0)
+        {
+            totoRigidbody.transform.localScale = new Vector3(1, 1, 1);
+        }
+        else
+        {
+            totoRigidbody.transform.localScale = new Vector3(-1, 1, 1);
+        }
     }
+
+
 }
