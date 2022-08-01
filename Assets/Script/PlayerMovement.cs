8dd873d3568f30ea44686021af790e79cccc6cd6
Shivam Maurya <109658264+ShivamManiMaurya@users.noreply.github.com>
2022-08-01 08:45:38 +0530

    Run Animation and Player Flip added

--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -7,12 +7,17 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] float totoSpeed = 5;
 
+    bool facingRight = true;
+
     Vector2 moveInput;
     Rigidbody2D totoRigidbody;
+    Animator totoAnimator;
+    
 
     void Start()
     {
         totoRigidbody = GetComponent<Rigidbody2D>();
+        totoAnimator = GetComponent<Animator>();
     }
 
     void Update()
@@ -30,19 +35,36 @@ public class PlayerMovement : MonoBehaviour
     {
         Vector2 playerVelocity = new Vector2(totoSpeed * moveInput.x, totoRigidbody.velocity.y);
         totoRigidbody.velocity = playerVelocity;
-        Debug.Log(totoRigidbody.velocity);
 
-        bool val = totoRigidbody.velocity.x > 0;
-        Debug.Log(val);
+        bool val = Mathf.Abs(totoRigidbody.velocity.x) > Mathf.Epsilon;
+        totoAnimator.SetBool("isRunning", val);
 
-        if (totoRigidbody.velocity.x >= 0)
+        // This method also works, but it continuously changes the scale.
+        //if (Mathf.Abs(totoRigidbody.velocity.x) > 0)
+        //{
+        //    Debug.Log(totoRigidbody.transform.localScale);
+        //    totoRigidbody.transform.localScale = new Vector2(Mathf.Sign(totoRigidbody.velocity.x), 1f);
+        //}
+
+
+        if (totoRigidbody.velocity.x > 0 && !facingRight)
         {
-            totoRigidbody.transform.localScale = new Vector3(1, 1, 1);
+            FlipPlayer();
         }
-        else
+        if (totoRigidbody.velocity.x < 0 && facingRight)
         {
-            totoRigidbody.transform.localScale = new Vector3(-1, 1, 1);
+            FlipPlayer();
         }
+    }
+
+    void FlipPlayer()
+    {
+        Vector3 currentScale = totoRigidbody.transform.localScale;
+        currentScale.x *= -1;
+        totoRigidbody.transform.localScale = currentScale;
+        Debug.Log(totoRigidbody.transform.localScale);
+
+        facingRight = !facingRight;
     }
 
 
