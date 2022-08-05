b267167c6d167c8f27691d2d120954359f4ab331
Shivam Maurya <109658264+ShivamManiMaurya@users.noreply.github.com>
2022-08-05 14:14:07 +0530

    Pickups.cs script is added
    By this we can pickup coins and other objects

---
+++ Assets/Script/Pickups.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickups : MonoBehaviour
+{
+   
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+        }
+    }
+
+}
