8eaf281c6f67e142899c2d52cc5384d2f7ce5437
Shivam Maurya <109658264+ShivamManiMaurya@users.noreply.github.com>
2022-08-06 19:45:32 +0530

    Buttons Added

---
+++ Assets/Script/ManiMenu.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ManiMenu : MonoBehaviour
+{
+    [SerializeField] Sprite clickedButtonSprite;
+    [SerializeField] Button button;
+    [SerializeField] TextMeshPro buttonText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ChangeButtonSprite()
+    {
+        button.image.sprite = clickedButtonSprite;
+        //buttonText.alignment = 
+        buttonText.text = "Play";
+        
+    }
+
+}
